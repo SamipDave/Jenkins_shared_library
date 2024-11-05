0203e79efb233fbcaa4053e6692d17b5b20886fb
SamipDave <118797360+SamipDave@users.noreply.github.com>
2024-11-05 17:10:27 +0000

    Update dockerbuild.groovy
    add

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -2,6 +2,5 @@ def call(String project, String ImageTag, String hubUser){
     sh """
     docker image build -t ${hubUser}/${project} .
     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
